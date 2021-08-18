0f0a50ed9186dd31a6b39ea9d1fb60c3f95669f5
len standvoss <lenstandvoss@yahoo.de>
2021-08-19 00:11:48 +0200

    Create seperate hub for chat and add clear method

---
+++ SyncStreamAPI/Hubs/ChatHub.cs
@@ -0,0 +1,69 @@
+﻿using SyncStreamAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SyncStreamAPI.Hubs
+{
+    public partial class ServerHub
+    {
+        public async Task SendMessage(ChatMessage message, string UniqueId)
+        {
+            Room room = GetRoom(UniqueId);
+            if (room == null)
+                return;
+            Server MainServer = room.server;
+            message.time = DateTime.Now;
+            MainServer.chatmessages.Add(message);
+            if (MainServer.chatmessages.Count >= 100)
+                MainServer.chatmessages = MainServer.chatmessages.GetRange(MainServer.chatmessages.Count - 100, MainServer.chatmessages.Count);
+            await Clients.Group(UniqueId).sendmessage(MainServer.chatmessages);
+        }
+
+        public async Task SendPrivateMessage(string UniqueId, string FromUser, string ToUser, string Message)
+        {
+            try
+            {
+                Room room = GetRoom(UniqueId);
+                if (room == null)
+                    return;
+                Server MainServer = room.server;
+                Member FromIdx = MainServer.members.FirstOrDefault(x => x.username == FromUser);
+                Member ToIdx = MainServer.members.FirstOrDefault(x => x.username == ToUser);
+                if (FromIdx != null && ToIdx != null)
+                {
+                    string FullMessage = FromIdx.AddMessage(ToUser, Message);
+                    await Clients.Caller.PrivateMessage(FullMessage);
+                    await Clients.Client(ToIdx.ConnectionId).PrivateMessage(FullMessage);
+                }
+                else
+                {
+                    throw new Exception("User was not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                await Clients.Caller.dialog(new Dialog() { Header = "Message Error", Question = "There has been an error trying to send your message, please try again.", Answer1 = "Ok" });
+            }
+        }
+        public async Task ClearChat(string UniqueId)
+        {
+            try
+            {
+                Room room = GetRoom(UniqueId);
+                if (room == null)
+                    return;
+                Server MainServer = room.server;
+                MainServer.chatmessages = new List<ChatMessage>();
+                await Clients.Group(UniqueId).sendmessage(MainServer.chatmessages);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                await Clients.Caller.dialog(new Dialog() { Header = "Message Error", Question = "There has been an error trying to send your message, please try again.", Answer1 = "Ok" });
+            }
+        }
+    }
+}
--- SyncStreamAPI/Hubs/ServerHub.cs
+++ SyncStreamAPI/Hubs/ServerHub.cs
@@ -381,47 +381,6 @@ namespace SyncStreamAPI.Hubs
             await Clients.All.getrooms(DataManager.GetRooms());
         }
 
-        public async Task SendMessage(ChatMessage message, string UniqueId)
-        {
-            Room room = GetRoom(UniqueId);
-            if (room == null)
-                return;
-            Server MainServer = room.server;
-            message.time = DateTime.Now;
-            MainServer.chatmessages.Add(message);
-            if (MainServer.chatmessages.Count >= 100)
-                MainServer.chatmessages = MainServer.chatmessages.GetRange(MainServer.chatmessages.Count - 100, MainServer.chatmessages.Count);
-            await Clients.Group(UniqueId).sendmessage(MainServer.chatmessages);
-        }
-
-        public async Task SendPrivateMessage(string UniqueId, string FromUser, string ToUser, string Message)
-        {
-            try
-            {
-                Room room = GetRoom(UniqueId);
-                if (room == null)
-                    return;
-                Server MainServer = room.server;
-                Member FromIdx = MainServer.members.FirstOrDefault(x => x.username == FromUser);
-                Member ToIdx = MainServer.members.FirstOrDefault(x => x.username == ToUser);
-                if (FromIdx != null && ToIdx != null)
-                {
-                    string FullMessage = FromIdx.AddMessage(ToUser, Message);
-                    await Clients.Caller.PrivateMessage(FullMessage);
-                    await Clients.Client(ToIdx.ConnectionId).PrivateMessage(FullMessage);
-                }
-                else
-                {
-                    throw new Exception("User was not found");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                await Clients.Caller.dialog(new Dialog() { Header = "Message Error", Question = "There has been an error trying to send your message, please try again.", Answer1 = "Ok" });
-            }
-        }
-
         public async Task Ping(DateTime date)
         {
             await Clients.Caller.PingTest(date);
