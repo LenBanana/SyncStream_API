9f10f2918ac92acbf53c816cf1221cf26e25bfb9
len standvoss <lenstandvoss@yahoo.de>
2022-10-31 01:22:58 +0100

    add error catches for video get

--- SyncStreamAPI/Controllers/VideoController.cs
+++ SyncStreamAPI/Controllers/VideoController.cs
@@ -38,14 +38,21 @@ namespace SyncStreamAPI.Controllers
         [HttpGet("[action]")]
         public async Task<IActionResult> videoByToken(int uniqueId, string token)
         {
-            var dbFile = _postgres.Files?.Where(x => x.FileKey != null && x.FileKey == token && x.ID == uniqueId).FirstOrDefault();
-            if (dbFile == null)
-                return StatusCode(StatusCodes.Status403Forbidden);
-            var path = General.FilePath + $"\\{dbFile.FileKey}{dbFile.FileEnding}";
-            if (!System.IO.File.Exists(path))
-                return StatusCode(StatusCodes.Status404NotFound);
-            var fileBytes = await System.IO.File.ReadAllBytesAsync(path);
-            return File(fileBytes, "application/octet-stream", dbFile.Name.EndsWith(dbFile.FileEnding) ? dbFile.Name : dbFile.Name + dbFile.FileEnding, true);
+            try
+            {
+                var dbFile = _postgres.Files?.Where(x => x.FileKey != null && x.FileKey == token && x.ID == uniqueId).FirstOrDefault();
+                if (dbFile == null)
+                    return StatusCode(StatusCodes.Status403Forbidden);
+                var path = General.FilePath + $"\\{dbFile.FileKey}{dbFile.FileEnding}";
+                if (!System.IO.File.Exists(path))
+                    return StatusCode(StatusCodes.Status404NotFound);
+                var fileBytes = await System.IO.File.ReadAllBytesAsync(path);
+                return File(fileBytes, "application/octet-stream", dbFile.Name.EndsWith(dbFile.FileEnding) ? dbFile.Name : dbFile.Name + dbFile.FileEnding, true);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [RequestFormLimits(ValueLengthLimit = int.MaxValue, MultipartBodyLengthLimit = int.MaxValue)]
@@ -53,34 +60,41 @@ namespace SyncStreamAPI.Controllers
         [HttpPost("[action]")]
         public async Task<IActionResult> addVideo(string token)
         {
-            if (Request.ContentLength <= 0 || Request.Form == null || Request.Form.Files == null || Request.Form.Files.Count <= 0)
-                return StatusCode(StatusCodes.Status403Forbidden);
-            var file = Request.Form.Files[0];
-            var dbUser = _postgres.Users?.Include(x => x.RememberTokens).Where(x => x.RememberTokens != null && x.RememberTokens.Any(y => y.Token == token)).FirstOrDefault();
-            if (dbUser == null)
-                return StatusCode(StatusCodes.Status403Forbidden);
-            RememberToken Token = dbUser?.RememberTokens.FirstOrDefault(x => x.Token == token);
-            if (Token == null)
-                return StatusCode(StatusCodes.Status403Forbidden);
-            if (dbUser.userprivileges >= 3)
+            try
             {
-                if (file.Length <= 0)
-                    return StatusCode(StatusCodes.Status405MethodNotAllowed);
-                using (var ms = new MemoryStream())
+                if (Request.ContentLength <= 0 || Request.Form == null || Request.Form.Files == null || Request.Form.Files.Count <= 0)
+                    return StatusCode(StatusCodes.Status403Forbidden);
+                var file = Request.Form.Files[0];
+                var dbUser = _postgres.Users?.Include(x => x.RememberTokens).Where(x => x.RememberTokens != null && x.RememberTokens.Any(y => y.Token == token)).FirstOrDefault();
+                if (dbUser == null)
+                    return StatusCode(StatusCodes.Status403Forbidden);
+                RememberToken Token = dbUser?.RememberTokens.FirstOrDefault(x => x.Token == token);
+                if (Token == null)
+                    return StatusCode(StatusCodes.Status403Forbidden);
+                if (dbUser.userprivileges >= 3)
                 {
-                    await file.CopyToAsync(ms);
-                    var bytes = ms.ToArray();
-                    var dbfile = new DbFile(file.FileName, "." + file.FileName.Split('.').Last(), dbUser);
-                    var path = General.FilePath + $"\\{dbfile.FileKey}{dbfile.FileEnding}";
-                    if (!Directory.Exists(General.FilePath))
-                        Directory.CreateDirectory(General.FilePath);
-                    System.IO.File.WriteAllBytes(path, bytes);
-                    _postgres.Files?.Add(dbfile);
-                    await _postgres.SaveChangesAsync();
-                    return Ok();
+                    if (file.Length <= 0)
+                        return StatusCode(StatusCodes.Status405MethodNotAllowed);
+                    using (var ms = new MemoryStream())
+                    {
+                        await file.CopyToAsync(ms);
+                        var bytes = ms.ToArray();
+                        var dbfile = new DbFile(file.FileName, "." + file.FileName.Split('.').Last(), dbUser);
+                        var path = General.FilePath + $"\\{dbfile.FileKey}{dbfile.FileEnding}";
+                        if (!Directory.Exists(General.FilePath))
+                            Directory.CreateDirectory(General.FilePath);
+                        System.IO.File.WriteAllBytes(path, bytes);
+                        _postgres.Files?.Add(dbfile);
+                        await _postgres.SaveChangesAsync();
+                        return Ok();
+                    }
                 }
+                return StatusCode(StatusCodes.Status403Forbidden);
             }
-            return StatusCode(StatusCodes.Status403Forbidden);
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
     }
 }
