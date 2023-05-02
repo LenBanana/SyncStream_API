95e7c96eb8f068f786447d0a3530611f52e150c8
len standvoss <lenstandvoss@yahoo.de>
2023-05-02 02:13:49 +0200

    Add the new "DateToBeDeleted" to the DTO object

--- SyncStreamAPI/DTOModel/FileDto.cs
+++ SyncStreamAPI/DTOModel/FileDto.cs
@@ -11,6 +11,7 @@ namespace SyncStreamAPI.DTOModel
         public string FileEnding { get; set; }
         public string FileKey { get; set; }
         public DateTime Created { get; set; }
+        public DateTime? DateToBeDeleted { get; set; }
         public int FileFolderId { get; set; }
         public bool Temporary { get; set; }
         public long Length => !System.IO.File.Exists($"{(Temporary ? General.TemporaryFilePath : General.FilePath)}/{FileKey}{FileEnding}") ? 0 : new System.IO.FileInfo($"{(Temporary ? General.TemporaryFilePath : General.FilePath)}/{FileKey}{FileEnding}").Length;
@@ -24,6 +25,7 @@ namespace SyncStreamAPI.DTOModel
             Created = file.Created;
             FileFolderId = file.DbFileFolderId;
             Temporary = file.Temporary;
+            DateToBeDeleted = file.DateToBeDeleted;
         }
     }
 }
